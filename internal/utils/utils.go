dac9ec42f6672e5b7e142cbd0999625c5285f5ef
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-07-30 22:49:47 +0200

    feat: add unique job id to logs and http response to track deployments

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -11,10 +11,11 @@ type jsonError struct {
 	Error      string `json:"error"`
 	Details    string `json:"details,omitempty"`
 	Repository string `json:"repository,omitempty"`
+	JobID      string `json:"job_id,omitempty"`
 }
 
 // JSONError writes an error response to the client in JSON format
-func JSONError(w http.ResponseWriter, err interface{}, details, repo string, code int) {
+func JSONError(w http.ResponseWriter, err interface{}, details, repo, jobId string, code int) {
 	if _, ok := err.(error); ok {
 		err = fmt.Sprintf("%v", err)
 	}
@@ -24,6 +25,7 @@ func JSONError(w http.ResponseWriter, err interface{}, details, repo string, cod
 		Code:       code,
 		Details:    details,
 		Repository: repo,
+		JobID:      jobId,
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
