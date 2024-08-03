b5921419bf7913aa44c22f9016ebc5686c9d4c77
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-08-03 22:35:17 +0200

    refactor: adjust build opts (#52)
    * fix: docker build deployment
    
    * fix: docker build deployment
    
    * fix: docker build deployment
    
    * feat: add build options to deploy config
    
    * feat: add build options to deploy config
    
    * refactor: adjust build opts

--- internal/config/deploy_config.go
+++ internal/config/deploy_config.go
@@ -28,10 +28,10 @@ type DeployConfig struct {
 	Timeout          int      `yaml:"timeout" default:"300"`                                                                                        // Timeout is the time in seconds to wait for the deployment to finish in seconds before timing out
 	BuildOpts        struct {
 		ForceImagePull bool              `yaml:"force_image_pull" default:"false"` // ForceImagePull always attempt to pull a newer version of the image
-		Quiet          bool              `yaml:"quiet" default:"false"`            // Quiet suppresses the build output and only shows the final image ID
-		Args           map[string]string `yaml:"args"`                             // BuildArgs is a map of build-time variables
+		Quiet          bool              `yaml:"quiet" default:"false"`            // Quiet suppresses the build output
+		Args           map[string]string `yaml:"args"`                             // BuildArgs is a map of build-time arguments to pass to the build process
 		MemoryLimit    int64             `yaml:"memory_limit" default:"0"`         // MemoryLimit is the maximum amount of memory the build process may consume
-		NoCache        bool              `yaml:"no_cache" default:"false"`         // NoCache disables the use of the cache when building the images
+		NoCache        bool              `yaml:"no_cache" default:"false"`         // NoCache disables the use of the cache when building images
 	} `yaml:"build_opts"` // BuildOpts is the build options for the deployment
 }
 
