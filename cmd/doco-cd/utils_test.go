6f902dfaf3eaaa8c5feb5bd49808d6b81be8b983
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2024-08-07 10:31:55 +0200

    test: refactor tests

---
+++ cmd/doco-cd/utils_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestJSONResponse(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	jobId := uuid.Must(uuid.NewRandom()).String()
+
+	JSONResponse(rr, "this is a test", jobId, http.StatusOK)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+
+	expectedReturnMessage := fmt.Sprintf(`{"details":"this is a test","job_id":"%s"}%s`, jobId, "\n")
+	if rr.Body.String() != expectedReturnMessage {
+		t.Errorf("handler returned unexpected body: got '%v' want '%v'",
+			rr.Body.String(), expectedReturnMessage)
+	}
+}
+
+func TestJSONError(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	jobId := uuid.Must(uuid.NewRandom()).String()
+
+	JSONError(rr, "this is a error", "this is a detail", jobId, http.StatusInternalServerError)
+
+	if rr.Code != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			rr.Code, http.StatusInternalServerError)
+	}
+
+	expectedReturnMessage := fmt.Sprintf(`{"error":"this is a error","details":"this is a detail","job_id":"%s"}%s`, jobId, "\n")
+	if rr.Body.String() != expectedReturnMessage {
+		t.Errorf("handler returned unexpected body: got '%v' want '%v'",
+			rr.Body.String(), expectedReturnMessage)
+	}
+}
