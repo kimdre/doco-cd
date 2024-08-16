ec56a57907da397a66c8b0fcda69b04ce738112e
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2024-08-16 11:44:12 +0200

    test: add tests for http handler

---
+++ cmd/doco-cd/http_handler_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/docker/compose/v2/pkg/api"
+	"github.com/docker/compose/v2/pkg/compose"
+	"github.com/kimdre/doco-cd/internal/config"
+	"github.com/kimdre/doco-cd/internal/docker"
+	"github.com/kimdre/doco-cd/internal/logger"
+	"github.com/kimdre/doco-cd/internal/webhook"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"regexp"
+	"testing"
+)
+
+const (
+	githubPayloadFile = "testdata/github_payload.json"
+)
+
+// Make http call to HealthCheckHandler
+func TestHandlerData_HealthCheckHandler(t *testing.T) {
+	expectedResponse := fmt.Sprintln(`{"details":"healthy"}`)
+	expectedStatusCode := http.StatusOK
+
+	appConfig, err := config.GetAppConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log := logger.New(12)
+
+	dockerCli, err := docker.CreateDockerCli(appConfig.DockerQuietDeploy, !appConfig.SkipTLSVerification)
+	if err != nil {
+		t.Fatalf("Failed to create docker client: %v", err)
+	}
+
+	t.Cleanup(func() {
+		err = dockerCli.Client().Close()
+		if err != nil {
+			return
+		}
+	})
+
+	h := handlerData{
+		dockerCli: dockerCli,
+		appConfig: appConfig,
+		log:       log,
+	}
+
+	req, err := http.NewRequest("GET", healthPath, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(h.HealthCheckHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != expectedStatusCode {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, expectedStatusCode)
+	}
+
+	if rr.Body.String() != expectedResponse {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expectedResponse)
+	}
+}
+
+func TestHandlerData_WebhookHandler(t *testing.T) {
+	expectedResponse := `{"details":"project deployment successful","job_id":"[a-f0-9-]{36}"}`
+	expectedStatusCode := http.StatusCreated
+
+	payload, err := os.ReadFile(githubPayloadFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	minifiedPayload := new(bytes.Buffer)
+
+	err = json.Compact(minifiedPayload, payload)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	appConfig, err := config.GetAppConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log := logger.New(12)
+
+	dockerCli, err := docker.CreateDockerCli(appConfig.DockerQuietDeploy, !appConfig.SkipTLSVerification)
+	if err != nil {
+		t.Fatalf("Failed to create docker client: %v", err)
+	}
+
+	t.Cleanup(func() {
+		err = dockerCli.Client().Close()
+		if err != nil {
+			return
+		}
+	})
+
+	t.Cleanup(func() {
+		ctx := context.Background()
+
+		service := compose.NewComposeService(dockerCli)
+
+		downOpts := api.DownOptions{
+			RemoveOrphans: true,
+			Images:        "all",
+			Volumes:       true,
+		}
+
+		t.Log("Remove test container")
+
+		err = service.Down(ctx, "compose-webhook", downOpts)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	h := handlerData{
+		dockerCli: dockerCli,
+		appConfig: appConfig,
+		log:       log,
+	}
+
+	req, err := http.NewRequest("POST", webhookPath, bytes.NewReader(payload))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set(webhook.GithubSignatureHeader, "sha256="+webhook.GenerateHMAC(payload, appConfig.WebhookSecret))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(h.WebhookHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != expectedStatusCode {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, expectedStatusCode)
+	}
+
+	regex, err := regexp.Compile(expectedResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !regex.MatchString(rr.Body.String()) {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expectedResponse)
+	}
+}
