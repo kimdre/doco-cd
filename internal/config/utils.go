c20ef65ea2b42cfff29f81ccfe7c1e54099a0fb8
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2025-05-09 13:31:21 +0200

    refactor: remove unused CamelCaseToSnakeCase function from utils

--- internal/config/utils.go
+++ internal/config/utils.go
@@ -4,12 +4,9 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"github.com/creasty/defaults"
 	"io"
 	"os"
-	"regexp"
-	"strings"
-
-	"github.com/creasty/defaults"
 
 	"gopkg.in/yaml.v3"
 )
@@ -85,15 +82,4 @@ func loadFileBasedEnvVars(cfg *AppConfig) error {
 	}
 
 	return nil
-}
-
-// CamelCaseToSnakeCase converts a string from camelCase to snake_case.
-func CamelCaseToSnakeCase(str string) string {
-	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
-	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
-
-	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-
-	return strings.ToLower(snake)
 }
