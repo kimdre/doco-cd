35296a07b1fa4c3e307c88c1a1f9fdf471f69441
Kim Oliver Drechsel <kim@drechsel.xyz>
2025-05-13 23:22:42 +0200

    fix: include stack name in JSON error response details

--- cmd/doco-cd/response.go
+++ cmd/doco-cd/response.go
@@ -7,7 +7,7 @@ import (
 )
 
 type jsonResponse struct {
-	Details string `json:"details,omitempty"`
+	Details any    `json:"details,omitempty"`
 	JobID   string `json:"job_id,omitempty"`
 }
 
@@ -18,7 +18,7 @@ type jsonError struct {
 }
 
 // JSONError writes an error response to the client in JSON format
-func JSONError(w http.ResponseWriter, err interface{}, details, jobId string, code int) {
+func JSONError(w http.ResponseWriter, err interface{}, details any, jobId string, code int) {
 	if _, ok := err.(error); ok {
 		err = fmt.Sprintf("%v", err)
 	}
