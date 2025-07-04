6f0dd6fb2036e3ccad928d7a34807c5098aa0268
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2025-07-04 12:19:42 +0200

    fix: add hard reset to worktree during repository checkout

--- internal/git/git.go
+++ internal/git/git.go
@@ -154,6 +154,13 @@ func UpdateRepository(path, ref string, skipTLSVerify bool, proxyOpts transport.
 		return nil, fmt.Errorf("%w: %w: %s", ErrCheckoutFailed, err, refSet.localRef)
 	}
 
+	err = worktree.Reset(&git.ResetOptions{
+		Mode: git.HardReset,
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	return repo, nil
 }
 
