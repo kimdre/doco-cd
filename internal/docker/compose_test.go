d720453f2e810d1ea002f4458c5b17485abc786f
Bernd Schorgers <me@bjw-s.dev>
2025-11-22 08:40:12 +0100

    Fix linting issues

--- internal/docker/compose_test.go
+++ internal/docker/compose_test.go
@@ -220,7 +220,7 @@ func TestDeployCompose(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	deployConfigs, err := config.GetDeployConfigs(tmpDir, projectName, customTarget, p.Ref)
+	deployConfigs, err := config.GetDeployConfigs(tmpDir, c.DeployConfigBaseDir, projectName, customTarget, p.Ref)
 	if err != nil {
 		t.Fatal(err)
 	}
