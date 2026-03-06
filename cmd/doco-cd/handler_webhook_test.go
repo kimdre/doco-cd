2ae159a1d4bdadee5637299bcc2fe5b1fa3ecd21
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2026-03-06 15:14:13 +0100

    fix(tests): add logging for found containers in stack during webhook tests

--- cmd/doco-cd/handler_webhook_test.go
+++ cmd/doco-cd/handler_webhook_test.go
@@ -199,7 +199,7 @@ func TestHandlerData_WebhookHandler(t *testing.T) {
 		}
 
 		for _, c := range containers {
-			t.Logf("Found container in stack: %s with name %s", c.ID, c.Service)
+			t.Logf("Found container in stack: %s with name %s", c.ID, c.Name)
 
 			if c.Service == containerName {
 				testContainerID = c.ID
