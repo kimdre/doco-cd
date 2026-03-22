01ffb109c0fd5a7108922e49028f8d9ba351a61f
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2026-03-22 14:19:27 +0800

    feat(utils): set add Difference method

--- internal/utils/set/set_test.go
+++ internal/utils/set/set_test.go
@@ -1,6 +1,9 @@
 package set
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSet(t *testing.T) {
 	t.Parallel()
@@ -39,5 +42,66 @@ func TestSet(t *testing.T) {
 	// Test Contains
 	if !s.Contains("banana") || !s.Contains("cherry") {
 		t.Errorf("expected set to contain 'banana' and 'cherry'")
+	}
+}
+
+func TestSet_Difference(t *testing.T) {
+	tests := []struct {
+		name  string
+		s     Set[string]
+		other Set[string]
+		want  Set[string]
+	}{
+		{
+			name: "other include all in s",
+			s: New(
+				"apple",
+			),
+			other: New(
+				"apple",
+				"banana",
+				"cherry",
+			),
+			want: New[string](),
+		},
+		{
+			name: "s include all in other",
+			s: New(
+				"apple",
+				"banana",
+				"cherry",
+			),
+			other: New(
+				"apple",
+			),
+			want: New(
+				"banana",
+				"cherry",
+			),
+		},
+		{
+			name: "s same with other",
+			s: New(
+				"apple",
+			),
+			other: New(
+				"apple",
+			),
+			want: New[string](),
+		},
+		{
+			name:  "s same with other and empty",
+			s:     New[string](),
+			other: New[string](),
+			want:  New[string](),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.s.Difference(tt.other)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Difference() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
