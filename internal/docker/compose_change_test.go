e3fd8fddbbfec583109723c550187865f280ab2e
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2026-03-22 21:20:05 +0800

    fix(test): t.Parallel called multiple times

--- internal/docker/compose_change_test.go
+++ internal/docker/compose_change_test.go
@@ -395,8 +395,8 @@ func Test_getChangeAndIgnore(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Parallel()
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got, got2 := getChangeAndIgnore(tt.changed, tt.ignored)
 			slices.Sort(got)
 			slices.Sort(got2)
