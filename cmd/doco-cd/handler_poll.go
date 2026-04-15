dcb84b6478d20a215c23f65576d1a43fdb63f0e0
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2026-04-15 21:06:00 +0800

    chore: make lint

--- cmd/doco-cd/handler_poll.go
+++ cmd/doco-cd/handler_poll.go
@@ -52,7 +52,6 @@ func StartPoll(h *handlerData, pollConfig config.PollConfig, wg *sync.WaitGroup)
 	h.log.Debug("Starting poll handler", "config", pollConfig)
 
 	wg.Go(func() {
-
 		h.PollHandler(context.Background(), pollJob)
 		h.log.Debug("PollJob handler stopped", "config", pollConfig)
 	})
