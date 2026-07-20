4eed0818abb2322d2b7ac170c3b7d0f1d41f4598
Kim Oliver Drechsel <kim@drechsel.xyz>
2026-07-20 21:15:11 +0200

    fix(api): ensure missing API key gets rejected

--- internal/restapi/api.go
+++ internal/restapi/api.go
@@ -15,7 +15,7 @@ var (
 // ValidateApiKey checks if the provided API key matches the one in the request header.
 func ValidateApiKey(r *http.Request, apiKey string) bool {
 	if apiKey == "" {
-		return true
+		return false
 	}
 
 	return r.Header.Get(KeyHeader) == apiKey
