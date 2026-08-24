61967d15dfcc6b507595023aebc185916987e4a8
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2026-08-24 16:52:42 +0200

    fix(e2e): implement discard logger for test container logging

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -5,9 +5,17 @@ package e2e
 import (
 	"os"
 	"testing"
+
+	tclog "github.com/testcontainers/testcontainers-go/log"
 )
 
+type discardLogger struct{}
+
+func (discardLogger) Printf(string, ...any) {}
+
 func TestMain(m *testing.M) {
+	tclog.SetDefault(discardLogger{})
+
 	code := m.Run()
 
 	teardownSuiteHarnesses()
