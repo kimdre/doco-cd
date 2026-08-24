1598882812399a9d10b74bb99cf6c535d4b7a64c
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2026-08-24 16:55:55 +0200

    Revert "fix(e2e): implement discard logger for test container logging"
    This reverts commit 61967d15dfcc6b507595023aebc185916987e4a8.

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -5,17 +5,9 @@ package e2e
 import (
 	"os"
 	"testing"
-
-	tclog "github.com/testcontainers/testcontainers-go/log"
 )
 
-type discardLogger struct{}
-
-func (discardLogger) Printf(string, ...any) {}
-
 func TestMain(m *testing.M) {
-	tclog.SetDefault(discardLogger{})
-
 	code := m.Run()
 
 	teardownSuiteHarnesses()
